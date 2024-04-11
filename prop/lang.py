4cfcb02ecdee6188108d922410cd56f87d93d0c3
= <38575855+fishy15@users.noreply.github.com>
2024-04-10 19:28:13 -0500

    Add function to convert prop exprs to strings

--- prop/lang.py
+++ prop/lang.py
@@ -13,10 +13,16 @@ Id: TypeAlias = str
 class EVar(Expr):
     name: Id
 
+    def __str__(self) -> str:
+        return f"({self.name})"
+
 
 @dataclass(frozen=True, slots=True, eq=True)
 class ENeg(Expr):
     body: Expr
+
+    def __str__(self) -> str:
+        return f"(!{self.body})"
 
 
 @dataclass(frozen=True, slots=True, eq=True)
@@ -24,14 +30,23 @@ class EImplies(Expr):
     left: Expr
     right: Expr
 
+    def __str__(self) -> str:
+        return f"({self.left} -> {self.right})"
+
 
 @dataclass(frozen=True, slots=True, eq=True)
 class EAnd(Expr):
     left: Expr
     right: Expr
 
+    def __str__(self) -> str:
+        return f"({self.left} && {self.right})"
+
 
 @dataclass(frozen=True, slots=True, eq=True)
 class EOr(Expr):
     left: Expr
     right: Expr
+
+    def __str__(self) -> str:
+        return f"({self.left} || {self.right})"
--- tests/test_prop_parser.py
+++ tests/test_prop_parser.py
@@ -16,3 +16,17 @@ from prop.prop_parser import parse_prop
 )
 def test_prop_parser(expr: str, expected: Expr) -> None:
     assert parse_prop(expr) == expected
+
+
+@pytest.mark.parametrize(
+    "expr,expected",
+    [
+        (EImplies(EVar("P"), EVar("Q")), "((P) -> (Q))"),
+        (EAnd(EVar("P"), EVar("Q")), "((P) && (Q))"),
+        (EOr(EVar("P"), EVar("Q")), "((P) || (Q))"),
+        (ENeg(EVar("P")), "(!(P))"),
+        (ENeg(ENeg(EVar("P"))), "(!(!(P)))"),
+    ],
+)
+def test_prop_str(expr: Expr, expected: str) -> None:
+    assert str(expr) == expected
