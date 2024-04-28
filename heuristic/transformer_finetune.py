2f21783f94efe5bad47a99e352b5ac19a7e6d292
= <38575855+fishy15@users.noreply.github.com>
2024-04-27 21:07:05 -0500

    Update requirements.txt

--- heuristic/transformer_finetune.py
+++ heuristic/transformer_finetune.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn.functional as F
 
 from datasets import Dataset
-from prop.tactics import all_tactics
 from transformers import (
     RobertaTokenizerFast,
     Trainer,
